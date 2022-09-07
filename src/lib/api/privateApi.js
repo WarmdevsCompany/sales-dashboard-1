62eab7918164ee791711d854f793a511eca72bb0
mac <mac@macs-MacBook-Pro.local>
2022-09-07 15:28:32 +0300

    add new private api fetch

--- src/lib/api/privateApi.js
+++ src/lib/api/privateApi.js
@@ -14,3 +14,18 @@ export function privateApi(resource, esiToken) {
 		goto('/auth/login');
 	}
 }
+
+export function privateApiWithBody(resource, data, esiToken) {
+	if (esiToken) {
+		return fetch(`${variables.privatePath}/${resource}`, {
+			method: 'POST',
+			headers: {
+				accept: 'application/json',
+				Authorization: esiToken
+			},
+			body: data && JSON.stringify(data)
+		});
+	} else {
+		goto('/auth/login');
+	}
+}
