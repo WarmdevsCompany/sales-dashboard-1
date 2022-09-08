11cdb6352b76081a58067540e39cd2174299aa3d
mac <mac@macs-MacBook-Pro.local>
2022-09-08 10:17:48 +0300

    add changes to publicApi

--- src/lib/api/publicApi.js
+++ src/lib/api/publicApi.js
@@ -1,11 +1,20 @@
 import { variables } from '$lib/variables';
 
 export function publicApi(method, resource, data) {
-	return fetch(`${variables.publicPath}/${resource}`, {
-		method,
-		headers: {
-			'content-type': 'application/json'
-		},
-		body: data && JSON.stringify(data)
-	});
+	if (data) {
+		return fetch(`${variables.publicPath}/${resource}`, {
+			method,
+			headers: {
+				'content-type': 'application/json'
+			},
+			body: data && JSON.stringify(data)
+		});
+	} else {
+		return fetch(`${variables.publicPath}/${resource}`, {
+			method,
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+	}
 }
