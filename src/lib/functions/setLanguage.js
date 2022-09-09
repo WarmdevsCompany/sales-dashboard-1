e875d4f2e76254be9939de31934c99e17e7ad3ab
Maxym Balagur <maxymbalagur@MacBook-Pro-Maxym.local>
2022-09-09 10:51:33 +0300

    lang post to server

--- src/lib/functions/setLanguage.js
+++ src/lib/functions/setLanguage.js
@@ -1,3 +1,22 @@
-export function setLanguage(lang) {
+export function setLanguage(lang, langId) {
     localStorage.setItem('lang', lang);
+    setLanguagePost(langId);
+}
+
+async function setLanguagePost(langId) {
+    const responseRaw = await fetch('/api/changeLang', {
+        method: 'POST',
+        headers: {
+            accept: 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({ langId: langId })
+    });
+    let response = await responseRaw.json();
+
+    if (!response.status) {
+        errorMessages = response.errorMessage;
+        console.log(errorMessages)
+    } 
 }
---
+++ src/routes/api/changeLang/+server.js
@@ -0,0 +1,24 @@
+import { variables } from '$lib/variables';
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ locals, request }) {
+	const body = await request.json();
+	const resource = 'changeLang';
+	const rawResponse = await fetch(`${variables.privatePath}/${resource}`, {
+		method: 'POST',
+		headers: {
+			accept: 'application/json',
+			Authorization: locals.esiToken
+		},
+		body: body && JSON.stringify(body)
+	});
+	const response = await rawResponse.json();
+	if (!response.status) {
+		const res = {
+			errors: {
+				message: response.errorMessage
+			}
+		};
+		return new Response(JSON.stringify(res));
+	}
+	return new Response(JSON.stringify(response));
+}
