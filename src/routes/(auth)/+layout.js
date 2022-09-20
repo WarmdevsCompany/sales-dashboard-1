b33c68a22d12f5edcb44a93177378ff09c789b97
ilia1997 <ilia@warmdevs.com>
2022-09-20 18:48:20 +0300

    add error page

--- src/routes/(auth)/+layout.js
+++ src/routes/(auth)/+layout.js
@@ -2,15 +2,20 @@ import { locale } from '$lib/translations/i18n.js';
 import { browser } from '$app/environment';
 import { redirect } from '@sveltejs/kit';
 import { getCookie } from '$lib/functions/getCookie';
+import { error } from '@sveltejs/kit';
 
-export function load() {
+/** @type {import('./$types').PageLoad} */
+export function load({ url, status }) {
 	if (browser) {
 		let lang = localStorage.getItem('lang');
 		locale.set(lang || 'EN');
 		const token = getCookie('esiToken');
-
 		if (token) {
 			throw redirect(307, '/overview/general');
+		}else if(!token && url.pathname != '/auth/login' && url.pathname != '/auth/reset-password'){
+			 throw redirect(307, '/auth/login');
 		}
 	}
 }
+
+ 
